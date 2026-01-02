140382632989a0d08210580da397fc99af252d9d
suhasnu <suhas3908@gmail.com>
2026-01-02 18:30:33 +0100

    setup

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+from typing import List
+
+Hypen_e_dot = '-e .'
+
+def get_requirements(file_path:str) -> List[str]:
+  '''
+  This function will return the list of requirements
+  '''
+  requirements=[]
+  with open(file_path) as file_obj:
+    requirements = file_obj.readlines()
+    requirements = [req.replace("\n", "")for req in requirements]
+    
+    if Hypen_e_dot in requirements:
+      requirements.remove(Hypen_e_dot)
+      
+  return requirements
+
+setup(
+  name="end-to-end-ml-project",
+  version='0.0.1',
+  author="Suahs",
+  author_email='suhas3908@gmail.com',
+  packages=find_packages(),
+  install_requires = get_requirements('requirements.txt')  
+  
+)
