7654080d36b904970cef97611cc2f12cc1b57fec
suhasnu <suhas3908@gmail.com>
2026-01-03 17:58:44 +0100

    Prediction pipeline done

--- src/utils.py
+++ src/utils.py
@@ -17,6 +17,8 @@ def save_object(file_path, obj):
       
   except Exception as e:
     raise CustomException(e, sys)
+  
+
 
 def evaluate_models(X_train, y_train, X_test, y_test, models, param):
   try:
@@ -43,6 +45,15 @@ def evaluate_models(X_train, y_train, X_test, y_test, models, param):
     
     return report
 
+  except Exception as e:
+    raise CustomException(e, sys)
+  
+
+def load_object(file_path):
+  try:
+    with open(file_path, "rb") as file_obj:
+      return dill.load(file_obj)
+  
   except Exception as e:
     raise CustomException(e, sys)
     
